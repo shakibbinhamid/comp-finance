1684d67e78526b86e3768745eb8416c3d150b729
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-16 14:32:33 +0000

    I think this one is better

---
+++ lab 1/q_2.m
@@ -0,0 +1,155 @@
+
+% if we want to experience a smaller portfolio
+%returns = returns(:,12:20);
+
+% returns need to be changed to percentages
+% for each return, covert it from
+% [100 120 150 100]
+% to
+% [0% 20% 50% 0%]
+for i=1:size(returns,2)
+    value = returns(1,i);
+    returns(:,i) = (returns(:,i) - value)/value;
+end
+% now remove the first opservation (it is only zeros
+% as it was used to convert returns to percentages)
+returns = returns(2:end,:);
+
+nAssets = size(returns,2);
+nTotal = size(returns,1);
+nTrain = int16(nTotal/2);
+nTest = nTotal - nTrain;
+
+% split the returns to train and test
+returnsTrain = returns(1:nTrain,:);
+returnsTest = returns(nTrain+1:nTotal,:);
+
+% get mean and covariance for these returns
+rMean = mean(returnsTrain);
+rCovar = cov(returnsTrain);
+
+% simluated returns, help for testing
+%returnsTrainSim = mvnrnd(rMean, rCovar, nTotal);
+
+% calcuate efficient forntier
+p = Portfolio;
+p = setAssetMoments(p, rMean, rCovar);
+p = setDefaultConstraints(p);
+
+fWeights = estimateFrontier(p, 30)';
+[fRisk, fReturn] = estimatePortMoments(p, fWeights);
+
+nPortfolios = size(fWeights,1);
+
+% also calcuate the efficient frontier for test data
+% just for the sake of comparison
+%[fRiskSim, fReturnSim, fWeightsSim] = frontcon(mean(returnsTest), cov(returnsTest));
+
+% get the weights for one portfolio using naiive method (i.e 1/n)
+% i.e. portfolio consists of assets with the same weights
+naiveWeights = ones(1, nAssets) *(1/nAssets);
+
+% calcuate the returns for the test data using the 2 different
+% weights: the efficient-frontier weights, and the naiive weights
+returnsEfficient = zeros(nTest, nPortfolios);
+for i=1:nPortfolios
+    returnsEfficient(:,i) = returnsTest * fWeights(i,:)';
+end
+returnsNaive = returnsTest*naiveWeights';
+
+% get the average of all efficient returns
+% notice, we only want to get the average on non
+returnsEfficientAverage = zeros(nTest, 1);
+for i=1:nTest
+    returnsEfficientAverage(i) = mean(returnsEfficient(i,:));
+end
+
+% now, we want to calcuate the Sharpe Ratio for the returns
+% with risk free = 5%
+riskFree = 20/100;
+sharpeEfficient = zeros(1, nPortfolios);
+for i=1:nPortfolios
+    sharpeEfficient(i) = (mean(returnsEfficient(:,i)) - riskFree)/std(returnsEfficient(:,i));
+end
+sharpeNaive = (mean(returnsNaive) - riskFree)/std(returnsNaive);
+sharpeEfficientAverage = mean(sharpeEfficient);
+
+% % visualize the pair-wise correlation
+% % for the pair-wise returns of 2 stocks
+% figure(1);clf;
+% box on;
+% grid on;
+% hold on;
+% daspect([1 1 1]);
+% plot(returnsTrain(:,1), returnsTrain(:,2), '.r');
+% plot(returnsTrain(:,2), returnsTrain(:,3), '.b');
+% plot(returnsTrain(:,1), returnsTrain(:,3), '.k');
+% plot(returnsTest(:,1), returnsTest(:,2), '.g');
+% plot(returnsTest(:,2), returnsTest(:,3), '.g');
+% plot(returnsTest(:,1), returnsTest(:,3), '.g');
+% 
+% % visualize the pair-wise correlation but
+% % on the simulated return
+% figure(2);clf;
+% box on;
+% grid on;
+% hold on;
+% daspect([1 1 1]);
+% plot(returnsTrainSim(:,1), returnsTrainSim(:,2), '.r');
+% plot(returnsTrainSim(:,2), returnsTrainSim(:,3), '.b');
+% plot(returnsTrainSim(:,1), returnsTrainSim(:,3), '.k');
+
+% plot the E-V graph (efficient frontier we get from the train data)
+figure(3); clf;
+box on;
+grid on;
+hold on;
+%plot(fRiskSim, fReturnSim, 'r', 'LineWidth', 3);
+plot(fRisk, fReturn, 'k', 'LineWidth', 2);
+plot(fRisk, fReturn, '.r', 'MarkerSize', 20);
+xlabel('Risk (V)', 'FontSize', 18);
+ylabel('Expected Return (E)', 'FontSize', 18);
+title('Efficient Portfolio Frontier', 'FontSize', 18);
+fig_legend = legend('Efficient Frontier', 'Efficient Portfolio', 'Location', 'southeast');
+set(fig_legend,'FontSize',16);
+
+% plot the returns for the efficient protfolios
+% vs. return from the naive portfolio
+colormap = autumn(nPortfolios+2);
+colormap = colormap(1:end-2,:);
+figure(4); clf;
+box on;
+grid on;
+hold on;
+plot(returnsNaive, 'b', 'LineWidth', 3);
+plot(returnsEfficientAverage, 'LineWidth', 3, 'Color', [0 0.7 0.2]);
+returnsEfficient = fliplr(returnsEfficient);
+for i=1:nPortfolios
+    plot(returnsEfficient(:,i), 'LineWidth', 1, 'Color', colormap(i,:));
+end
+returnsEfficient = fliplr(returnsEfficient);
+xlabel('Time (Days)', 'FontSize', 18);
+ylabel('Return (%)', 'FontSize', 18);
+title('Portfolio Return Over Time', 'FontSize', 18);
+fig_legend = legend('Naive Portfolio', 'Efficient Portfolio Avg.', 'Efficient Portfolios', 'Location', 'northwest');
+set(fig_legend,'FontSize',16);
+
+% plot the sharpe values
+colormap = autumn(nPortfolios);
+figure(5); clf;
+box on;
+grid on;
+hold on;
+plot([1 nPortfolios], [sharpeNaive sharpeNaive], 'LineWidth', 2, 'Color', 'b');
+plot([1 nPortfolios], [sharpeEfficientAverage sharpeEfficientAverage], 'LineWidth', 2, 'Color', [0 0.7 0.2]);
+sharpeEfficient = fliplr(sharpeEfficient);
+for i=1:nPortfolios
+    plot(i, sharpeEfficient(i), '.r', 'MarkerSize', 30, 'Color', colormap(i,:));
+    plot(i, sharpeEfficient(i), '.k', 'MarkerSize', 10);
+end
+sharpeEfficient = fliplr(sharpeEfficient);
+xlabel('Portfolio', 'FontSize', 18);
+ylabel('Ratio', 'FontSize', 18);
+title(strcat('Sharpe Ratio - Risk Free:', int2str(riskFree*100) ,'%' ), 'FontSize', 18);
+fig_legend = legend('Naive Portfolio', 'Efficient Portfolio Avg.', 'Efficient Portfolios', 'Location', 'southwest');
+set(fig_legend,'FontSize',14);
--- load_fin_data.m
+++ load_fin_data.m
@@ -1,39 +1,39 @@
 %% -------------------------------------------------------------
-data_dir = '/home/shakibbinhamid/Documents/comp fin/lab 1 2015/data/Updated/';
+data_dir = '/home/shakibbinhamid/Documents/MATLAB/comp-finance/data/Updated/';
 
 %% -------------------------------------------------------------
 ftse = csvread(strcat(data_dir, '^FTSE.csv'), 1, 6);
 
-stocks(:, 1) = csvread(strcat(data_dir, 'AAL.L.csv'), 1, 6);
-stocks(:, 2) = csvread(strcat(data_dir, 'AV.L.csv'), 1, 6);
-stocks(:,3) = csvread(strcat(data_dir, 'BA.L.csv'), 1, 6);
-stocks(:,4) = csvread(strcat(data_dir, 'BARC.L.csv'), 1, 6);
-stocks(:,5) = csvread(strcat(data_dir, 'BLT.L.csv'), 1, 6);
-stocks(:,6) = csvread(strcat(data_dir, 'BP.L.csv'), 1, 6);
-stocks(:,7) = csvread(strcat(data_dir, 'BT-A.L.csv'), 1, 6);
-stocks(:,8) = csvread(strcat(data_dir, 'CNA.L.csv'), 1, 6);
-stocks(:,9) = csvread(strcat(data_dir, 'GLEN.L.csv'), 1, 6);
-stocks(:,10) = csvread(strcat(data_dir, 'GSK.L.csv'), 1, 6);
-stocks(:,11) = csvread(strcat(data_dir, 'HSBA.L.csv'), 1, 6);
-stocks(:,12) = csvread(strcat(data_dir, 'ITV.L.csv'), 1, 6);
-stocks(:,13) = csvread(strcat(data_dir, 'KGF.L.csv'), 1, 6);
-stocks(:,14) = csvread(strcat(data_dir, 'LGEN.L.csv'), 1, 6);
-stocks(:,15) = csvread(strcat(data_dir, 'LLOY.L.csv'), 1, 6);
-stocks(:,16) = csvread(strcat(data_dir, 'MKS.L.csv'), 1, 6);
-stocks(:,17) = csvread(strcat(data_dir, 'MRW.L.csv'), 1, 6);
-stocks(:,18) = csvread(strcat(data_dir, 'NG.L.csv'), 1, 6);
-stocks(:,19) = csvread(strcat(data_dir, 'OML.L.csv'), 1, 6);
-stocks(:,20) = csvread(strcat(data_dir, 'RBS.L.csv'), 1, 6);
-stocks(:,21) = csvread(strcat(data_dir, 'RDSB.L.csv'), 1, 6);
-stocks(:,22) = csvread(strcat(data_dir, 'RIO.L.csv'), 1, 6);
-stocks(:,23) = csvread(strcat(data_dir, 'RR.L.csv'), 1, 6);
-stocks(:,24) = csvread(strcat(data_dir, 'SBRY.L.csv'), 1, 6);
-stocks(:,25) = csvread(strcat(data_dir, 'SGE.L.csv'), 1, 6);
-stocks(:,26) = csvread(strcat(data_dir, 'SKY.L.csv'), 1, 6);
-stocks(:,27) = csvread(strcat(data_dir, 'STAN.L.csv'), 1, 6);
-stocks(:,28) = csvread(strcat(data_dir, 'TSCO.L.csv'), 1, 6);
-stocks(:,29) = csvread(strcat(data_dir, 'TW.L.csv'), 1, 6);
-stocks(:,30) = csvread(strcat(data_dir, 'VOD.L.csv'), 1, 6);
+returns(:, 1) = csvread(strcat(data_dir, 'AAL.L.csv'), 1, 6);
+returns(:, 2) = csvread(strcat(data_dir, 'AV.L.csv'), 1, 6);
+returns(:,3) = csvread(strcat(data_dir, 'BA.L.csv'), 1, 6);
+returns(:,4) = csvread(strcat(data_dir, 'BARC.L.csv'), 1, 6);
+returns(:,5) = csvread(strcat(data_dir, 'BLT.L.csv'), 1, 6);
+returns(:,6) = csvread(strcat(data_dir, 'BP.L.csv'), 1, 6);
+returns(:,7) = csvread(strcat(data_dir, 'BT-A.L.csv'), 1, 6);
+returns(:,8) = csvread(strcat(data_dir, 'CNA.L.csv'), 1, 6);
+returns(:,9) = csvread(strcat(data_dir, 'GLEN.L.csv'), 1, 6);
+returns(:,10) = csvread(strcat(data_dir, 'GSK.L.csv'), 1, 6);
+returns(:,11) = csvread(strcat(data_dir, 'HSBA.L.csv'), 1, 6);
+returns(:,12) = csvread(strcat(data_dir, 'ITV.L.csv'), 1, 6);
+returns(:,13) = csvread(strcat(data_dir, 'KGF.L.csv'), 1, 6);
+returns(:,14) = csvread(strcat(data_dir, 'LGEN.L.csv'), 1, 6);
+returns(:,15) = csvread(strcat(data_dir, 'LLOY.L.csv'), 1, 6);
+returns(:,16) = csvread(strcat(data_dir, 'MKS.L.csv'), 1, 6);
+returns(:,17) = csvread(strcat(data_dir, 'MRW.L.csv'), 1, 6);
+returns(:,18) = csvread(strcat(data_dir, 'NG.L.csv'), 1, 6);
+returns(:,19) = csvread(strcat(data_dir, 'OML.L.csv'), 1, 6);
+returns(:,20) = csvread(strcat(data_dir, 'RBS.L.csv'), 1, 6);
+returns(:,21) = csvread(strcat(data_dir, 'RDSB.L.csv'), 1, 6);
+returns(:,22) = csvread(strcat(data_dir, 'RIO.L.csv'), 1, 6);
+returns(:,23) = csvread(strcat(data_dir, 'RR.L.csv'), 1, 6);
+returns(:,24) = csvread(strcat(data_dir, 'SBRY.L.csv'), 1, 6);
+returns(:,25) = csvread(strcat(data_dir, 'SGE.L.csv'), 1, 6);
+returns(:,26) = csvread(strcat(data_dir, 'SKY.L.csv'), 1, 6);
+returns(:,27) = csvread(strcat(data_dir, 'STAN.L.csv'), 1, 6);
+returns(:,28) = csvread(strcat(data_dir, 'TSCO.L.csv'), 1, 6);
+returns(:,29) = csvread(strcat(data_dir, 'TW.L.csv'), 1, 6);
+returns(:,30) = csvread(strcat(data_dir, 'VOD.L.csv'), 1, 6);
 
 %% -------------------------------------------------------------
 
@@ -41,7 +41,6 @@ stocks(:,30) = csvread(strcat(data_dir, 'VOD.L.csv'), 1, 6);
 % R = price2ret(stocks);
 
 y = tick2ret(ftse);
-R = tick2ret(stocks);
+R = tick2ret(returns);
 
 [T, N] = size(R);
-
