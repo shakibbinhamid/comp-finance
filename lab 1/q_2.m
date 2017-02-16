164e080612924288f270ed86c83056eff0d75260
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-16 21:55:58 +0000

    doing q2

--- lab 1/q_2.m
+++ lab 1/q_2.m
@@ -5,12 +5,12 @@ load_fin_data;
 % returns is T x N portfolio returns where T = time ticks, N = #assets
 % row(i+1) = (row(i) - row(1))/row(1)
 % remove row(1) since it's just 0's
+% then select n columns or assets to look at. here n = 3
 
 first_invest = returns(1, :);
-for i=1:size(returns,1)
-    returns(i,:) = (returns(i, :) - first_invest) ./ first_invest;
-end
+returns = (returns - first_invest) ./ first_invest;
 returns = returns(2:end,:);
+returns = returns(:,randperm(30, 3));
 
 %% ------- calculate the training and testing data in 50-50 ratio ---------
 nAssets = size(returns,2);
@@ -26,32 +26,41 @@ returnsTest = returns(nTrain+1:nTotal,:);
 rMean = mean(returnsTrain);
 rCovar = cov(returnsTrain);
 
-% simluated returns, help for testing
-%returnsTrainSim = mvnrnd(rMean, rCovar, nTotal);
+%% ---- efficient markowitz portfolio based on data from the first half ---
+num_points = 100;
 
 % calcuate efficient forntier
 p = Portfolio;
 p = setAssetMoments(p, rMean, rCovar);
 p = setDefaultConstraints(p);
 
-fWeights = estimateFrontier(p, 30)';
-[fRisk, fReturn] = estimatePortMoments(p, fWeights);
+effWeights = estimateFrontier(p, num_points);
+[effRisk, effReturn] = estimatePortMoments(p, effWeights);
+effWeights = effWeights';
 
-nPortfolios = size(fWeights,1);
+% plot the E-V graph (efficient frontier we get from the train data)
+figure(1); clf;
+box on;
+grid on;
+hold on;
+plotFrontier(p, num_points);
+xlabel('Risk (V)', 'FontSize', 18);
+ylabel('Expected Return (E)', 'FontSize', 18);
+title('Efficient Portfolio Frontier', 'FontSize', 18);
+fig_legend = legend('Efficient Frontier', 'Location', 'southeast');
+set(fig_legend,'FontSize',16);
 
-% also calcuate the efficient frontier for test data
-% just for the sake of comparison
-%[fRiskSim, fReturnSim, fWeightsSim] = frontcon(mean(returnsTest), cov(returnsTest));
+%% -------- naive 1/N portfolio based on data from the first half ---------
 
-% get the weights for one portfolio using naiive method (i.e 1/n)
-% i.e. portfolio consists of assets with the same weights
 naiveWeights = ones(1, nAssets) *(1/nAssets);
+
+%% --------- calculate returns for both naive and eff portfolio -----------
 
 % calcuate the returns for the test data using the 2 different
 % weights: the efficient-frontier weights, and the naiive weights
-returnsEfficient = zeros(nTest, nPortfolios);
-for i=1:nPortfolios
-    returnsEfficient(:,i) = returnsTest * fWeights(i,:)';
+returnsEfficient = zeros(nTest, num_points);
+for i=1:num_points
+    returnsEfficient(:,i) = returnsTest * effWeights(i,:)';
 end
 returnsNaive = returnsTest*naiveWeights';
 
@@ -65,8 +74,8 @@ end
 % now, we want to calcuate the Sharpe Ratio for the returns
 % with risk free = 5%
 riskFree = 20/100;
-sharpeEfficient = zeros(1, nPortfolios);
-for i=1:nPortfolios
+sharpeEfficient = zeros(1, num_points);
+for i=1:num_points
     sharpeEfficient(i) = (mean(returnsEfficient(:,i)) - riskFree)/std(returnsEfficient(:,i));
 end
 sharpeNaive = (mean(returnsNaive) - riskFree)/std(returnsNaive);
@@ -97,23 +106,11 @@ sharpeEfficientAverage = mean(sharpeEfficient);
 % plot(returnsTrainSim(:,2), returnsTrainSim(:,3), '.b');
 % plot(returnsTrainSim(:,1), returnsTrainSim(:,3), '.k');
 
-% plot the E-V graph (efficient frontier we get from the train data)
-figure(3); clf;
-box on;
-grid on;
-hold on;
-%plot(fRiskSim, fReturnSim, 'r', 'LineWidth', 3);
-plot(fRisk, fReturn, 'k', 'LineWidth', 2);
-plot(fRisk, fReturn, '.r', 'MarkerSize', 20);
-xlabel('Risk (V)', 'FontSize', 18);
-ylabel('Expected Return (E)', 'FontSize', 18);
-title('Efficient Portfolio Frontier', 'FontSize', 18);
-fig_legend = legend('Efficient Frontier', 'Efficient Portfolio', 'Location', 'southeast');
-set(fig_legend,'FontSize',16);
+
 
 % plot the returns for the efficient protfolios
 % vs. return from the naive portfolio
-colormap = autumn(nPortfolios+2);
+colormap = autumn(num_points+2);
 colormap = colormap(1:end-2,:);
 figure(4); clf;
 box on;
@@ -122,7 +119,7 @@ hold on;
 plot(returnsNaive, 'b', 'LineWidth', 3);
 plot(returnsEfficientAverage, 'LineWidth', 3, 'Color', [0 0.7 0.2]);
 returnsEfficient = fliplr(returnsEfficient);
-for i=1:nPortfolios
+for i=1:num_points
     plot(returnsEfficient(:,i), 'LineWidth', 1, 'Color', colormap(i,:));
 end
 returnsEfficient = fliplr(returnsEfficient);
@@ -133,15 +130,15 @@ fig_legend = legend('Naive Portfolio', 'Efficient Portfolio Avg.', 'Efficient Po
 set(fig_legend,'FontSize',16);
 
 % plot the sharpe values
-colormap = autumn(nPortfolios);
+colormap = autumn(num_points);
 figure(5); clf;
 box on;
 grid on;
 hold on;
-plot([1 nPortfolios], [sharpeNaive sharpeNaive], 'LineWidth', 2, 'Color', 'b');
-plot([1 nPortfolios], [sharpeEfficientAverage sharpeEfficientAverage], 'LineWidth', 2, 'Color', [0 0.7 0.2]);
+plot([1 num_points], [sharpeNaive sharpeNaive], 'LineWidth', 2, 'Color', 'b');
+plot([1 num_points], [sharpeEfficientAverage sharpeEfficientAverage], 'LineWidth', 2, 'Color', [0 0.7 0.2]);
 sharpeEfficient = fliplr(sharpeEfficient);
-for i=1:nPortfolios
+for i=1:num_points
     plot(i, sharpeEfficient(i), '.r', 'MarkerSize', 30, 'Color', colormap(i,:));
     plot(i, sharpeEfficient(i), '.k', 'MarkerSize', 10);
 end
