b11a6169dd03c5415284b1dda8e0919a372d5e58
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-20 12:08:23 +0000

    done with q1

--- lab 1/q1_d.m
+++ lab 1/q1_d.m
@@ -1,12 +1,9 @@
 m = [ 0.10, 0.20, 0.15 ]';
-C = 100 * [
-      [  0.005, -0.010,  0.004 ]
-      [ -0.010,  0.040, -0.002 ]
-      [  0.004, -0.002,  0.023 ]
-    ];
+C = [0.005, -0.010,  0.004 ;-0.010,  0.040, -0.002 ;0.004, -0.002,  0.023 ];
+N = 10;
 
-[PRisk1, PRoR1, PWts1] = naiveMV(m, C, 10);
-[PRisk2, PRoR2, PWts2] = naiveMV_CVX(m, C, 10);
+[PRisk1, PRoR1, PWts1] = naiveMV(m, C, N);
+[PRisk2, PRoR2, PWts2] = naiveMV_CVX(m, C, N);
 plot(PRisk1, PRoR1);
 hold on;
 plot(PRisk2, PRoR2, 'b*');
