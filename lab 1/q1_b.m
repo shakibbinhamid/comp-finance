c8c19e19b912058f08489d3720a9855cc07206fe
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-20 11:13:24 +0000

    finalising q1

--- lab 1/q1_b.m
+++ lab 1/q1_b.m
@@ -1,103 +1,72 @@
-clc;
-% we have 3-asset portfolio, we'll divide them to 3 2-asset protfolios
-% and draw the efficient frontier and guassian dist. for them
+clc; clearvars;
 
-% mean and covariance for all the assets the portfolio
+%% given ------------------------------------------------------------------
+
 mean_3assets = [0.1; 0.2; 0.15];
-covariance_3assets = 100 * [0.005, -0.010, 0.004; -0.010 0.040 -0.002; 0.004, -0.002, 0.023];
-
-% these different mean and covariance to see how is the effect
-% of changing the mean and covariance on the forntier
-% mean_3assets = [0.1; 0.2; 0.185];
-%covariance_3assets = 100 * [0.005, -0.010, 0.004; -0.010 0.040 -0.002; 0.004, -0.002, 0.08];
-
+covariance_3assets = [0.005, -0.010, 0.004; -0.010 0.040 -0.002; 0.004, -0.002, 0.023];
 N = 100;
 
-useCVX = false;
+%% generate the 2 combinations of mean and cov and eff portfolio ----------
 
-effRisks = [];
-effReturns = [];
-effWeights = [];
-m = zeros(1, 3);
-c = zeros(1, 3);
+nAssets = size(mean_3assets, 1);
+
+eff_2_Risks = zeros(N, nAssets);
+eff_2_Returns = zeros(N, nAssets);
+eff_2_Weights = zeros(N, nAssets*(nAssets - 1));
 
 for i=1:3
     
     mean_2assets = mean_3assets;
     covariance_2assets = covariance_3assets;
-    indexToRemove = 0;
     
     switch(i)
-        % asset 1 and 2
         case 1
             indexToRemove = 3;
-            % asset 2 and 3
         case 2
             indexToRemove = 1;
-            % asset 1 and 3
         case 3
             indexToRemove = 2;
         otherwise
     end
     
-    % change 3d mean to 2d mean
-    % also change the 3*3 covariance matrix to 2*2 one
     mean_2assets(indexToRemove) = [];
     covariance_2assets(indexToRemove,:) = [];
     covariance_2assets(:,indexToRemove) = [];
     
-    % calc mean and standard deviation
-    m(1,i) = norm(mean_2assets);
-    c(1,i) = norm(covariance_2assets);
+    [effRisk, effReturn, effWeight] = naiveMV(mean_2assets, covariance_2assets, N);
     
-    % get the frontier for the 2-asset portfolio, the return values
-    % are the risk, return and weights of the portfolio
-    if (~useCVX)
-        [effRisk, effReturn, effWeight] = naiveMV(mean_2assets, covariance_2assets, N);
-    else
-        [effRisk, effReturn, effWeight] = naiveMV_CVX(mean_2assets, covariance_2assets, N);
-    end
-        
-    % another method to calcuate efficient forntier
-    %[effRisk, effReturn, effWeight] = frontcon(mean_2assets, covariance_2assets, N);
-    
-    effRisks = [effRisks effRisk];
-    effReturns = [effReturns effReturn];
-    effWeights = [effWeights effWeight];
+    eff_2_Risks(:, i) = effRisk;
+    eff_2_Returns(:, i) = effReturn;
+    eff_2_Weights(:, i:i+size(effWeight, 2) - 1) = effWeight;
 end
 
-% also, get the frontier for the 3-asset portfolio
+%% generate the total eff portfolio ---------------------------------------
 
-if (~useCVX)
-    [effRisk_3, effReturn_3, effWeight_3] = naiveMV(mean_3assets, covariance_3assets, N);
-else
-    [effRisk_3, effReturn_3, effWeight_3] = naiveMV_CVX(mean_3assets, covariance_3assets, N);
-end
+[eff_3_Risk, eff_3_Return, eff_3_Weight] = naiveMV(mean_3assets, covariance_3assets, N);
 
-% generate N random returns (normally distributed)
-% for the 3-asset system (portfolio)
-N = 600;
-returns = mvnrnd(mean_3assets, covariance_3assets, N);
 
-% plot the E-V graph
+%% plot the efficient portfolios ------------------------------------------
+
 figure(1); clf;
 box on;
 grid on;
 hold on;
-%daspect([15 1 1]);
-%axis tight;
-%axis([-0.2,2.3,0.09,0.21]);
-plot(effRisk_3(:), effReturn_3(:), '-.', 'LineWidth', 4, 'Color', [0.7 0.7 0.7]);
-plot(effRisks(:,1), effReturns(:,1), 'r', 'LineWidth', 2);
-plot(effRisks(:,2), effReturns(:,2), 'b', 'LineWidth', 2);
-plot(effRisks(:,3), effReturns(:,3), 'LineWidth', 2, 'Color', [0 0.7 0.2]);
+plot(eff_3_Risk(:), eff_3_Return(:), '-.', 'LineWidth', 4, 'Color', [0.7 0.7 0.7]);
+plot(eff_2_Risks(:,1), eff_2_Returns(:,1), 'r', 'LineWidth', 2);
+plot(eff_2_Risks(:,2), eff_2_Returns(:,2), 'b', 'LineWidth', 2);
+plot(eff_2_Risks(:,3), eff_2_Returns(:,3), 'LineWidth', 2, 'Color', [0 0.7 0.2]);
 xlabel('Risk', 'FontSize', 18);
 ylabel('Expected Return', 'FontSize', 18);
 title('Efficient Portfolio Frontiers', 'FontSize', 18);
 fig_legend = legend( 'Asset 1,2,3', 'Asset 1,2', 'Asset 2,3', 'Asset 1,3', 'Location', 'southeast');
 set(fig_legend,'FontSize',12);
 
-% plot the returns of the 3 models (each is 2-asset normal distrubution)
+%% generate N random returns for the 3 set portfolio ----------------------
+
+N = 600;
+returns = mvnrnd(mean_3assets, covariance_3assets, N);
+
+%% plot how the 2 assets are distributed ----------------------------------
 figure(2); clf;
 
 subplot(1,3,1);
@@ -105,7 +74,7 @@ plot(returns(:,1), returns(:,2), '.r');
 box on;
 grid on;
 daspect([1 1 1]);
-axis([-8,8,-8,8]);
+axis([-1,1,-1,1]);
 xlabel('Asset 1', 'FontSize', 14);
 ylabel('Asset 2', 'FontSize', 14);
 title('Portfolio 1 Distribution', 'FontSize', 14);
@@ -115,7 +84,7 @@ plot(returns(:,2), returns(:,3), '.b');
 box on;
 grid on;
 daspect([1 1 1]);
-axis([-8,8,-8,8]);
+axis([-1,1,-1,1]);
 xlabel('Asset 2', 'FontSize', 14);
 ylabel('Asset 3', 'FontSize', 14);
 title('Portfolio 2 Distribution', 'FontSize', 14);
@@ -125,7 +94,7 @@ plot(returns(:,1), returns(:,3), '.', 'Color', [0 0.7 0.2]);
 box on;
 grid on;
 daspect([1 1 1]);
-axis([-8,8,-8,8]);
+axis([-1,1,-1,1]);
 xlabel('Asset 1', 'FontSize', 14);
 ylabel('Asset 3', 'FontSize', 14);
 title('Portfolio 3 Distribution', 'FontSize', 14);
