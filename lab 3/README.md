749d1f61a0297f1727e3e52494ab64d04b780a6d
shakib bin hamid <sh3g12@soton.ac.uk>
2017-04-02 16:52:16 +0100

    initial commit

---
+++ lab 3/README.md
@@ -0,0 +1,2 @@
+Need to get libsvm and xtal_regression. follow their installation instructions and edit 
+includePaths to point to the right folders.
---
+++ lab 3/includePathes.m
@@ -0,0 +1,6 @@
+clc;
+
+addpath('/home/shakib/Documents/MATLAB/comp-finance/lab 3/src/Shared/');
+addpath('/home/shakib/Documents/MATLAB/comp-finance/lab 3/src/Questions/');
+addpath('/home/shakib/Documents/MATLAB/xtal_regression/');
+addpath('/home/shakib/Documents/MATLAB/libsvm/matlab');
